89a8d305e9a0b002a97c0a4130a45ec23a0c7e33
Sriramnark <64322927+rambo-94@users.noreply.github.com>
2020-07-01 22:50:21 -0500

    Created Project for microservices with all required dependencies

---
+++ src/main/java/com/springboot/microservices/bankapp/bankcustomersdemographic/BankcustomersdemographicApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.microservices.bankapp.bankcustomersdemographic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankcustomersdemographicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankcustomersdemographicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/microservices/bankapp/bankcustomersdemographic/BankcustomersdemographicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.microservices.bankapp.bankcustomersdemographic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankcustomersdemographicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
