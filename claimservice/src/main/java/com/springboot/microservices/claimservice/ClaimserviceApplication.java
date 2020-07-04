cdcc644d7166bdfc662da1b4a92a2708612dfe28
Sriramnark <64322927+rambo-94@users.noreply.github.com>
2020-07-03 23:39:42 -0500

    created claims microservice to handfle cliams

---
+++ claimservice/src/main/java/com/springboot/microservices/claimservice/ClaimserviceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.microservices.claimservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaimserviceApplication.class, args);
+    }
+
+}
---
+++ claimservice/src/test/java/com/springboot/microservices/claimservice/ClaimserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.microservices.claimservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
