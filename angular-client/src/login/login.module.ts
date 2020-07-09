1ea3c3c9a455436ef9312c6d13926b23d4be071f
Sriramnark <64322927+rambo-94@users.noreply.github.com>
2020-07-08 23:46:48 -0500

    Implemented login component with all required validations

---
+++ angular-client/src/login/login.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {LoginService} from './login.service';
+import {LoginRoutingModule} from './login-routing.module';
+
+import {HttpClientModule, HttpHeaders, HttpParams} from '@angular/common/http';
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  providers: [LoginService, HttpParams],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LoginRoutingModule,
+    HttpClientModule
+
+  ]
+})
+export class LoginModule { }
---
+++ angular-client/src/login/login/login.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, NgForm, Validators} from '@angular/forms';
+import {LoginService} from '../login.service';
+import {Login} from '../login';
+import {Router} from '@angular/router';
+
+function contains(input: FormControl) {
+
+  const hasCharacters = (a) => {
+    if (a.value.includes('@') || a.value.includes('-') && a.value.includes(',')) {
+      return true;
+    }
+
+  };
+
+  return hasCharacters(input) ? null : {noRequiredCharacters: true};
+
+}
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+ loginCred: Login;
+ errorMessage = false;
+ error: string;
+  constructor(private builder: FormBuilder, private api: LoginService, private route: Router ) {
+  }
+
+
+  loginDetails = this.builder.group({
+
+    username: ['', [ Validators.minLength(5), Validators.pattern('[a-zA-Z0-9^@^-^,^.]{3-7}')]],
+    password: ['', [Validators.minLength(6), Validators.required, contains]]
+
+
+  });
+
+
+  ngOnInit() {
+  }
+
+  onSubmit(value: NgForm) {
+
+    console.log(value);
+    this.api.checkUserCred(value.value.username).subscribe(login => {
+
+      this.errorMessage = login.password === value.value.password ?  true : false;
+      if ( this.errorMessage ) {
+         this.route.navigate(['/home']);
+       }
+      }, message => {
+          console.log(message);
+          this.error = 'error retrieving details ,Please try again';
+      }
+    );
+  }
+}
+
+
+
