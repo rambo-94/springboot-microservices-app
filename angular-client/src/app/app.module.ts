619365fe70d77dd215b20afe67ccf45da250211e
Sriramnark <64322927+rambo-94@users.noreply.github.com>
2020-07-09 23:49:08 -0500

    miscellaneous changes to get the application up and running

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,15 +1,20 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
+import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {LoginModule} from '../login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterModule,
+        AppRoutingModule,
+        LoginModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
