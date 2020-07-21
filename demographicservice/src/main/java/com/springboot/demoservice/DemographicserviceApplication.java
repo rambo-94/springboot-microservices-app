42e94627d5d224846a417bc7e8aa34792084dc17
Sriramnark <64322927+rambo-94@users.noreply.github.com>
2020-07-20 23:37:17 -0500

    Updated staterfile,added decorators for Eureka  and zuul support

--- demographicservice/src/main/java/com/springboot/demoservice/DemographicserviceApplication.java
+++ demographicservice/src/main/java/com/springboot/demoservice/DemographicserviceApplication.java
@@ -2,8 +2,12 @@ package com.springboot.demoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class DemographicserviceApplication {
 
     public static void main(String[] args) {
