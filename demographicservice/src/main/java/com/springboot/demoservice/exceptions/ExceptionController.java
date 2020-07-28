d8acde6e9ca416055e6437e256358ad4ee32de11
Sriramnark <64322927+rambo-94@users.noreply.github.com>
2020-07-28 00:00:06 -0500

    Added Exception Handler annotation

--- demographicservice/src/main/java/com/springboot/demoservice/exceptions/ExceptionController.java
+++ demographicservice/src/main/java/com/springboot/demoservice/exceptions/ExceptionController.java
@@ -5,10 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionController {
 
+
+    @ExceptionHandler(value
+            = { IllegalArgumentException.class, IllegalStateException.class })
     public ResponseEntity transactionFailed(){
 
         ObjectMapper objectMapper =new ObjectMapper();
