8f6190c85d0ff2a1f87a5073e569db78de9624b9
Sriramnark <64322927+rambo-94@users.noreply.github.com>
2020-08-14 23:48:27 -0500

    Configured Zuul Proxy

--- zuul-api-gateway/src/main/java/com/springboot/microservices/zuulapigateway/ZuulApiGatewayApplication.java
+++ zuul-api-gateway/src/main/java/com/springboot/microservices/zuulapigateway/ZuulApiGatewayApplication.java
@@ -2,8 +2,12 @@ package com.springboot.microservices.zuulapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class ZuulApiGatewayApplication {
 
     public static void main(String[] args) {
